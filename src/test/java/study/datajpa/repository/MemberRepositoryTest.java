806b2c2f5a892a924e28ba27f49f327d58f5f0e2
우경서 (Gyeongseo Woo) <ugs1575@gmail.com>
2023-04-19 08:52:05 +0900

    프로젝트 세팅

---
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -0,0 +1,35 @@
+package study.datajpa.repository;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Optional;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import study.datajpa.entity.Member;
+
+@SpringBootTest
+@Transactional
+class MemberRepositoryTest {
+
+	@Autowired MemberRepository memberRepository;
+
+	@Test
+	public void testMember() {
+		Member member = new Member("memberA");
+		Member savedMember = memberRepository.save(member);
+
+		Member findMember = memberRepository.findById(savedMember.getId()).get();
+
+		assertThat(findMember.getId()).isEqualTo(member.getId());
+		assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+		assertThat(findMember).isEqualTo(member);
+
+	}
+
+}
