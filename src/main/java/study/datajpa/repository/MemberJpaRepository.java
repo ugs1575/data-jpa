e0b99024559c765828ade74009d53a57c335d477
우경서 (Gyeongseo Woo) <ugs1575@gmail.com>
2023-04-29 22:29:21 +0900

    순수 JPA 페이징과 정렬

--- src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -1,5 +1,8 @@
 package study.datajpa.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
@@ -18,8 +21,54 @@ public class MemberJpaRepository {
 		return member;
 	}
 
+	public void delete(Member member) {
+		em.remove(member);
+	}
+
+	public List<Member> findAll() {
+		return em.createQuery("select m from Member m", Member.class)
+			.getResultList();
+	}
+
+	public Optional<Member> findById(Long id) {
+		Member member = em.find(Member.class, id);
+		return Optional.ofNullable(member);
+	}
+
+	public long count() {
+		return em.createQuery("select count(m) from Member m", Long.class)
+			.getSingleResult();
+	}
+
 	public Member find(Long id) {
 		return em.find(Member.class, id);
+	}
+
+	public List<Member> findByUsernameAndAgeGreaterThen(String username, int age) {
+		return em.createQuery("select m from Member m where m.username = :username and m.age > :age")
+			.setParameter("username", username)
+			.setParameter("age", age)
+			.getResultList();
+	}
+
+	public List<Member> findByUsername(String username) {
+		return em.createQuery("Member.findByUsername", Member.class)
+			.setParameter("username", username)
+			.getResultList();
+	}
+
+	public List<Member> findByPage(int age, int offset, int limit) {
+		return em.createQuery("select m from Member m where m.age = :age order by m.username desc")
+			.setParameter("age", age)
+			.setFirstResult(offset)
+			.setMaxResults(limit)
+			.getResultList();
+	}
+
+	public long totalCount(int age) {
+		return em.createQuery("select count(m) from Member m where m.age = :age", Long.class)
+			.setParameter("age", age)
+			.getSingleResult();
 	}
 
 }
